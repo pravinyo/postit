d35c94b026f21491ebeab99c7032c8bd391bfe7d
Pravin Tripathi <pravin.tripathi@ves.ac.in>
2016-11-23 23:59:32 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$username="bp6am";
-$password="bp6ampass";
-$host="localhost";
-$database="post";
+$username="your database username";
+$password="your password";
+$host="host url";
+$database="your database number";
 $connection=mysqli_connect($host,$username,$password,$database);
 if(mysqli_connect_errno())
   {
